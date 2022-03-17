f25946619dd1b0cdbac118d8f1df74531da38b61
luciam9 <62023568+luciam9@users.noreply.github.com>
2022-03-17 11:44:06 +0100

    dequenode

--- DoubleEndedQueue/src/main/java/doubleEndedQueue/DequeNode.java
+++ DoubleEndedQueue/src/main/java/doubleEndedQueue/DequeNode.java
@@ -1,44 +1,58 @@
 package doubleEndedQueue;
 
 /**
- * Class representing a node of a double-ended queue (deque). Each node has pointers to
- * the next and previous nodes.
- * The previous and next of the first and last node of the deque is null.
+ * Class representing a node of a double-ended queue (deque). Each node has pointers to the next and
+ * previous nodes. The previous and next of the first and last node of the deque is null.
  *
  * @param <T>
  */
 public class DequeNode<T> {
-    private T item ;
-    private T next ;
-    private T previous ;
 
+    private T item;
+    private DequeNode<T> next;
+    private DequeNode<T> previous;
+
+    // Setters
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public void setNext(DequeNode<T> next) {
+        this.next = next;
+    }
+
+    public void setPrevious(DequeNode<T> previous) {
+        this.previous = previous;
+    }
+
+    // Getters
     public T getItem() {
         return item;
     }
 
-    public T getNext() {
+    public DequeNode<T> getNext() {
         return next;
     }
 
-    public T getPrevious() {
+    public DequeNode<T> getPrevious() {
         return previous;
     }
 
-    public DequeNode(T item, T next, T previous) {
-        this.item = item ;
-        this.next = next ;
-        this.previous = previous ;
+    public DequeNode(T item, DequeNode<T> next, DequeNode<T> previous) {
+        this.item = item;
+        this.next = next;
+        this.previous = previous;
     }
 
     public boolean isFirstNode() {
-        return previous == null ;
+        return previous == null;
     }
 
     public boolean isLastNode() {
-        return next == null ;
+        return next == null;
     }
 
     public boolean isNotATerminalNode() {
-        return (!isFirstNode() && !isLastNode()) ;
+        return (!isFirstNode() && !isLastNode());
     }
 }
---
+++ DoubleEndedQueue/src/main/java/doubleEndedQueue/DoubleLinkedListQueue.java
@@ -0,0 +1,61 @@
+package doubleEndedQueue;
+
+public class DoubleLinkedListQueue implements DoubleEndedQueue{
+
+    private DequeNode first;
+    private DequeNode last;
+
+    public DoubleLinkedListQueue(){
+        first = null;
+        last = null;
+    }
+
+    @Override
+    public void append(DequeNode node) {
+        DequeNode nuevo = new DequeNode(node.getItem(), null, last);
+        this.last = nuevo;
+    }
+
+    @Override
+    public void appendLeft(DequeNode node) {
+        DequeNode nuevo = new DequeNode(node.getItem(), first, null);
+        this.first = nuevo;
+    }
+
+    @Override
+    public void deleteFirst() {
+        first = first.getNext();
+    }
+
+    @Override
+    public void deleteLast() {
+        DequeNode last = this.last;
+        DequeNode previousToLast = this.last.getPrevious();
+
+        this.last = previousToLast;
+        this.last.setNext(null);
+    }
+
+    @Override
+    public DequeNode peekFirst() {
+        return this.first;
+    }
+
+    @Override
+    public DequeNode peekLast() {
+        return this.last;
+    }
+
+    @Override
+    public int size() {
+        int size = 0;
+        DequeNode actual = this.first;
+
+        while (!actual.isLastNode()){
+            size++;
+            actual = actual.getNext();
+        }
+
+        return size;
+    }
+}
