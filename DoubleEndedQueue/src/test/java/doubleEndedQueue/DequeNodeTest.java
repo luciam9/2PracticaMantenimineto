84f2aeb2c9075138e52d6fcc7353dfa72c069fe1
luciam9 <62023568+luciam9@users.noreply.github.com>
2022-03-17 12:15:54 +0100

    node tests

--- DoubleEndedQueue/src/test/java/doubleEndedQueue/DequeNodeTest.java
+++ DoubleEndedQueue/src/test/java/doubleEndedQueue/DequeNodeTest.java
@@ -1,7 +1,77 @@
 package doubleEndedQueue;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DequeNodeTest {
 
+    private DequeNode node;
+
+    @BeforeEach
+    public void setNode(){
+
+        DequeNode first = new DequeNode(1, null, null);
+        node = new DequeNode(2, null, first);
+        first.setNext(node);
+        DequeNode last =  new DequeNode(3, null, node);
+        node.setNext(last);
+    }
+
+    @AfterEach
+    public void destroy(){
+        node = null;
+    }
+
+    @Test
+    public void shouldGetTypeReturnNextItem(){
+        int expectedValue = 3;
+        assertEquals(expectedValue, node.getNext().getItem());
+    }
+
+    @Test
+    public void shouldGetTypeReturnPreviousItem(){
+        int expectedValue = 1;
+        assertEquals(expectedValue, node.getPrevious().getItem());
+    }
+
+    @Test
+    public void firstNodePreviousItemIsNull(){
+        assertNull(node.getPrevious().getPrevious());
+    }
+
+    @Test
+    public void lastNodeNextItemIsNull(){
+        assertNull(node.getNext().getNext());
+    }
+
+    @Test
+    public void lastNodeFunctionReturnsTrueIfNodeIsLast(){
+       assertTrue(node.getNext().isLastNode());
+    }
+
+    @Test
+    public void lastNodeFunctionReturnsFalseIfNodeIsNotLast(){
+        assertFalse(node.isLastNode());
+    }
+
+    @Test
+    public void firstNodeFunctionReturnsTrueIfNodeIsFirst(){
+        assertTrue(node.getPrevious().isFirstNode());
+    }
+
+    @Test
+    public void firstNodeFunctionReturnsFalseIfNodeIsNotFirst(){
+        assertFalse(node.isFirstNode());
+    }
+
+    @Test
+    public void nodeReturnsTrueIfNotTerminal(){
+        assertTrue(node.isNotATerminalNode());
+    }
+
+    @Test
+    public void nodeReturnsFalseIfTerminal(){
+        assertFalse(node.getNext().isNotATerminalNode());
+    }
 }
