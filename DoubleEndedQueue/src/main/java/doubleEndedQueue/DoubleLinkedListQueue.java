4477142fdda4bdd60a6b2b83e8c99800377fa2f8
luciam9 <62023568+luciam9@users.noreply.github.com>
2022-03-20 13:04:54 +0100

    queue tests

--- DoubleEndedQueue/src/main/java/doubleEndedQueue/DoubleLinkedListQueue.java
+++ DoubleEndedQueue/src/main/java/doubleEndedQueue/DoubleLinkedListQueue.java
@@ -13,37 +13,56 @@ public class DoubleLinkedListQueue implements DoubleEndedQueue{
     @Override
     public void append(DequeNode node) {
 
+        if(!(node.isLastNode() && node.isFirstNode())){
+            throw new RuntimeException("ERROR: Given node is not valid");
+        }
         if(first==null && last==null){
-
-            if(!(node.isLastNode() && node.isFirstNode())){
-                throw new RuntimeException("ERROR: Given node is not valid");
-            }
             first=node;
             last = node;
         }else{
             DequeNode nuevo = new DequeNode(node.getItem(), null, last);
+            this.last.setNext(nuevo);
             this.last = nuevo;
         }
     }
 
     @Override
     public void appendLeft(DequeNode node) {
-        DequeNode nuevo = new DequeNode(node.getItem(), first, null);
-        this.first = nuevo;
+
+        if(!(node.isLastNode() && node.isFirstNode())){
+            throw new RuntimeException("ERROR: Given node is not valid");
+        }
+        if(first==null && last==null){
+            first=node;
+            last = node;
+        }else {
+            DequeNode nuevo = new DequeNode(node.getItem(), first, null);
+            this.first = nuevo;
+        }
     }
 
     @Override
     public void deleteFirst() {
-        first = first.getNext();
+
+        if(first==null && last==null){
+            throw new RuntimeException("ERROR: Empty queue");
+        }else {
+            first = first.getNext();
+        }
     }
 
     @Override
     public void deleteLast() {
-        DequeNode last = this.last;
-        DequeNode previousToLast = this.last.getPrevious();
 
-        this.last = previousToLast;
-        this.last.setNext(null);
+        if(first==null && last==null){
+            throw new RuntimeException("ERROR: Empty queue");
+        }else {
+            DequeNode last = this.last;
+            DequeNode previousToLast = this.last.getPrevious();
+
+            this.last = previousToLast;
+            this.last.setNext(null);
+        }
     }
 
     @Override
@@ -58,7 +77,11 @@ public class DoubleLinkedListQueue implements DoubleEndedQueue{
 
     @Override
     public int size() {
-        int size = 0;
+
+        if(first==null && last==null){
+            return 0;
+        }else {
+        int size = 1;
         DequeNode actual = this.first;
 
         while (!actual.isLastNode()){
@@ -67,5 +90,6 @@ public class DoubleLinkedListQueue implements DoubleEndedQueue{
         }
 
         return size;
+        }
     }
 }
--- DoubleEndedQueue/src/test/java/doubleEndedQueue/DoubleEndedQueueTest.java
+++ DoubleEndedQueue/src/test/java/doubleEndedQueue/DoubleEndedQueueTest.java
@@ -2,27 +2,114 @@ package doubleEndedQueue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoubleEndedQueueTest {
 
     private DoubleLinkedListQueue queue;
+    private DoubleLinkedListQueue emptyQueue;
 
     @BeforeEach
     public void setNode(){
 
-        DequeNode first = new DequeNode(1, null, null);
+        /*DequeNode first = new DequeNode(1, null, null);
         DequeNode node = new DequeNode(2, null, first);
         first.setNext(node);
         DequeNode last =  new DequeNode(3, null, node);
-        node.setNext(last);
+        node.setNext(last);*/
 
         queue = new DoubleLinkedListQueue();
+        queue.append(new DequeNode(1, null, null));
+        queue.append(new DequeNode(2, null, null));
+
+        emptyQueue = new DoubleLinkedListQueue();
     }
 
     @AfterEach
     public void destroy(){
-        
+        queue = null;
+        emptyQueue = null;
+    }
+
+    @Test
+    public void appendEmptyQueueWithValidNodeAddsNode(){
+        DequeNode node = new DequeNode(1, null, null);
+        emptyQueue.append(node);
+        assertEquals(node, emptyQueue.peekFirst());
+        assertEquals(node, emptyQueue.peekLast());
+    }
+
+    @Test
+    public void appendNotEmptyQueueWithValidNodeAddsNode(){
+        DequeNode node = new DequeNode(3, null, null);
+        queue.append(node);
+        assertEquals(node.getItem(), queue.peekLast().getItem());
+    }
+
+    @Test
+    public void appendNotEmptyQueueWithNotValidNodeThrowsException(){
+        DequeNode node = new DequeNode(3, new DequeNode(4, null, null), null);
+        assertThrows(RuntimeException.class, () -> queue.append(node));
+    }
+
+    @Test
+    public void appendLeftEmptyQueueWithValidNodeAddsNode(){
+        DequeNode node = new DequeNode(1, null, null);
+        emptyQueue.appendLeft(node);
+        assertEquals(node, emptyQueue.peekFirst());
+        assertEquals(node, emptyQueue.peekLast());
+    }
+
+    @Test
+    public void appendLeftNotEmptyQueueWithValidNodeAddsNode(){
+        DequeNode node = new DequeNode(3, null, null);
+        queue.appendLeft(node);
+        assertEquals(node.getItem(), queue.peekFirst().getItem());
+    }
+
+    @Test
+    public void appendLeftNotEmptyQueueWithNotValidNodeThrowsException(){
+        DequeNode node = new DequeNode(3, new DequeNode(4, null, null), null);
+        assertThrows(RuntimeException.class, () -> queue.appendLeft(node));
+    }
+
+    @Test
+    public void deleteFirstNotEmptyQueueDeletesFirstNode(){
+        DequeNode first = queue.peekFirst().getNext();
+        queue.deleteFirst();
+
+        assertEquals(first.getItem(), queue.peekFirst().getItem());
+    }
+
+    @Test
+    public void deleteFirstEmptyQueueThrowsException(){
+        assertThrows(RuntimeException.class, () -> emptyQueue.deleteFirst());
+    }
+
+    @Test
+    public void deleteLastNotEmptyQueueDeletesLastNode(){
+        DequeNode last = queue.peekLast().getPrevious();
+        queue.deleteLast();
+
+        assertEquals(last.getItem(), queue.peekLast().getItem());
+    }
+
+    @Test
+    public void deleteLastEmptyQueueThrowsException(){
+        assertThrows(RuntimeException.class, () -> emptyQueue.deleteLast());
+    }
+
+    @Test
+    public void sizeEmptyQueueIsZero(){
+
+        assertEquals(0, emptyQueue.size());
+    }
+
+    @Test
+    public void sizeNotEmptyQueueIsNumberOfElements(){
+
+        assertEquals(2, queue.size());
     }
 }
